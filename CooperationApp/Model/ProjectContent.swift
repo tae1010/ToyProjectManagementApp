ef0c5358147fbab23e0941f73310af9e683c589d
김정태 <tae101010@naver.com>
2022-05-19 18:15:40 +0900

    projectcontent배열에서 content값(dictionary)을 따로 저장하여 left/right버튼을 누를때 각 content에 저장된 value값 tableview에 reload

--- CooperationApp/Model/ProjectContent.swift
+++ CooperationApp/Model/ProjectContent.swift
@@ -9,6 +9,6 @@ import Foundation
 
 struct ProjectContent {
     var id: String // Project 구조체에 있는 id 저장
-    var count: Int // 몇번째 배열인지 저장하는함수
+    var countIndex: Int // 몇번째 배열인지 저장하는함수
     var content: Dictionary<String, [String]> // title과 content들을 저장할 dictionary
 }
--- CooperationApp/ViewControllers/Main/ProjectContent/ProjectViewController.swift
+++ CooperationApp/ViewControllers/Main/ProjectContent/ProjectViewController.swift
@@ -11,31 +11,32 @@ import FirebaseDatabase
 
 class ProjectViewController: UIViewController {
     
-    var projectContent = [ProjectContent]()
+    var projectContent = [ProjectContent]() // project model 배열
+    var content = [String]() //projectcontent model의 content.value값을 저장
     
     var ref: DatabaseReference! = Database.database().reference()
-    var id: String = ""
+    var id: String = "" // 프로젝트의 uuid값을 받을 변수
     var currentCount: Int = 0 //현재 페이지
-    var contentTitle: String?
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewWillAppear(_ animated: Bool) {
-        self.readDB()
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        print("viewdidload시작")
         
         self.tableView.delegate = self
         self.tableView.dataSource = self
         
         let tableViewNib = UINib(nibName: "ProjectContentCell", bundle: nil)
         self.tableView.register(tableViewNib, forCellReuseIdentifier: "ProjectContentCell")
+        self.readDB()
+        print(self.currentCount)
         
-        print(self.projectContent)
     }
     
+    //뒤로가기 버튼(Maintabbarview로 돌아감)
     @IBAction func backButton(_ sender: UIButton) {
         dismiss(animated: false)
     }
@@ -72,12 +73,23 @@ class ProjectViewController: UIViewController {
     @IBAction func moveLeft(_ sender: UIButton) {
         if currentCount > 0 {
             self.currentCount -= 1
+            self.readContents()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            print(self.currentCount)
         }
     }
     
     @IBAction func moveRight(_ sender: UIButton) {
-        if maxCount >= currentCount {
+        if self.projectContent.count - 1 > currentCount {
+
             self.currentCount += 1
+            DispatchQueue.main.async {
+                self.readContents()
+                self.tableView.reloadData()
+            }
+            print(currentCount)
         }
     }
     
@@ -97,22 +109,38 @@ extension ProjectViewController {
             
             guard let value = snapshot.value as? [Dictionary<String, [String]>] else {return}
             for content in value {
-                //몇번째 인덱스인지?
+                // content는 ["contents0": ["123","234","123"]] ["contents1": ["11", "2323"]]
+                // content배열에서 몇번째 배열인지 ProjectContent.count에 저장
                 guard let count = value.firstIndex(of: content) else { return }
-                print(count)
-                let pc = ProjectContent(id: self.id, count: count, content: content)
-                print(pc)
+                let pc = ProjectContent(id: self.id, countIndex: count, content: content)
                 self.projectContent.append(pc)
+                
             }
-            
+            //self.projectContent
+            //[CooperationApp.ProjectContent(id: "4CCE18B9-88F2-4C4C-AB22-C6F74E74FF3F", count: 0, content: ["contents0": ["123", "234", "123"]]),
+            //CooperationApp.ProjectContent(id: "4CCE18B9-88F2-4C4C-AB22-C6F74E74FF3F", count: 1, content: ["contents1": ["11", "2323"]])]
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
+            
+            print("readDB완료")
+            self.readContents()
             
         }) { error in
           print(error.localizedDescription)
         }
     }
+    
+    //readDB에서 저장시킨 projectContent모델에서 현재 페이지의 content.value값을 저장시키는 함수
+    private func readContents() {
+        for pc in self.projectContent{
+            if pc.countIndex == currentCount {
+                //2차원 배열을 1차원으로 바꿔줌
+                self.content = Array(pc.content.values.joined())
+            }
+        }
+    }
+    
 }
 
 extension ProjectViewController: UITableViewDelegate {
@@ -121,27 +149,16 @@ extension ProjectViewController: UITableViewDelegate {
 
 extension ProjectViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //첫번쨰 배열에 배열 갯수
-        return self.projectContent.count
+        
+        return self.content.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ProjectContentCell", for: indexPath) as! ProjectContentTableViewCell
-        let projectContent = projectContent[indexPath.row].content
         
-//        struct ProjectContent {
-//            var id: String // Project 구조체에 있는 id 저장
-//            var count: Int // 몇번째 배열인지 저장하는함수
-//            var content: Dictionary<String, [String]> // title과 content들을 저장할 dictionary
-//        }
-//        if projectContent.count == currentCount {
-//            for (key,value) in projectContent.content{
-//                self.contentTitle = key
-//                cell.content.text = value.first
-//            }
-//        }
-        print(projectContent)
+        cell.content.text = self.content[indexPath.row]
+
         cell.leftInset = 20
         cell.rightInset = 20
 
